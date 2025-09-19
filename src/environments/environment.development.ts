0396b7e7c950d545cb7782617d5e80e1059fabbe
tefaabdellatief <tefaabdellatief@users.noreply.github.com>
2025-09-19 09:01:02 +0000

    deploy: 2d39ac968185a31c4886f7f39c0b6eb24f0544e6

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://dkmhcwlymsjctwighxvj.supabase.co',
-  supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrbWhjd2x5bXNqY3R3aWdoeHZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNjQ3NTMsImV4cCI6MjA3MTc0MDc1M30.MMbypICa08kiuz0CAtvzemOtn81-sGuIWuvvLGoDuPk',
-};
